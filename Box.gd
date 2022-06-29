7502aad5dbe9cf2b42c8dd3b5067fc9eda84818b
gerep <daniel.gerep@protonmail.com>
2022-06-28 23:12:11 -0300

    first version

---
+++ Box.gd
@@ -0,0 +1,19 @@
+extends StaticBody2D
+
+var tile_size = 64
+export var speed = 3
+
+onready var tween = $Tween
+onready var ray = $RayCast2D
+
+func move(dir):
+	ray.cast_to = dir * tile_size
+	ray.force_raycast_update()
+	if !ray.is_colliding():
+		move_tween(dir)
+
+func move_tween(dir):
+	tween.interpolate_property(self, "position",
+		position, position + dir * tile_size,
+		1.0/speed, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+	tween.start()
---
+++ Player.gd
@@ -0,0 +1,42 @@
+extends Area2D
+
+onready var ray = $RayCast2D
+onready var tween = $Tween
+export var speed = 3
+
+var tile_size = 64
+
+var inputs = {
+	"ui_right": Vector2.RIGHT,
+	"ui_left": Vector2.LEFT,
+	"ui_up": Vector2.UP,
+	"ui_down": Vector2.DOWN
+}
+
+func _ready():
+	position = position.snapped(Vector2.ONE * tile_size)
+	#position += Vector2.ONE * tile_size/2
+
+func _unhandled_input(event):
+	if tween.is_active():
+		return
+	for dir in inputs.keys():
+		if event.is_action_pressed(dir):
+			move(dir) # "ui_down"
+
+func move(dir):
+	ray.cast_to = inputs[dir] * tile_size
+	ray.force_raycast_update()
+	if !ray.is_colliding():
+#		position += dir * tile_size
+		move_tween(dir)
+	else:
+		var collider = ray.get_collider()
+		if collider.is_in_group("boxes"):
+			collider.move(inputs[dir])
+
+func move_tween(dir):
+	tween.interpolate_property(self, "position",
+		position, position + inputs[dir] * tile_size,
+		1.0/speed, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+	tween.start()
