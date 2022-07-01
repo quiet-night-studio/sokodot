049c8ce24c7b05c6d50185070c0ddc1f5371f441
Extremos <extextremos@protonmail.com>
2022-07-01 16:24:50 -0400

    Menu Change
    Removed Restart button and replaced with Main Menu button

--- Pause_Menu.gd
+++ Pause_Menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _ready():
-	$Menu_Buttons/Restart_Button.grab_focus()
+	$Menu_Buttons/Levels_Button.grab_focus()
 
 func _on_Levels_Button_pressed():
 	get_tree().change_scene("res://Scenes/Level_select.tscn")
@@ -11,5 +11,5 @@ func _on_Quit_Button_pressed():
 	get_tree().quit()
 
 
-func _on_Restart_Button_pressed():
-	get_tree().reload_current_scene()
+func _on_MainMenu_Button_pressed():
+	get_tree().change_scene("res://Scenes/Menu.tscn")
