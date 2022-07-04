be6d27fe1b7caca28814982672a8b8f383b7b21b
gerep <daniel.gerep@protonmail.com>
2022-07-04 11:45:04 -0300

    Remove empty lines from Pause script

--- Pause_Menu.gd
+++ Pause_Menu.gd
@@ -6,10 +6,8 @@ func _ready():
 func _on_Levels_Button_pressed():
 	get_tree().change_scene("res://Scenes/Level_select.tscn")
 	
-
 func _on_Quit_Button_pressed():
 	get_tree().quit()
-
 
 func _on_MainMenu_Button_pressed():
 	get_tree().change_scene("res://Scenes/Menu.tscn")
