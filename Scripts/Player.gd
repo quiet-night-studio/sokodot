47f86698e002fc84b1b80d573dd6898594e1f0b0
gerep <daniel.gerep@protonmail.com>
2022-07-04 11:41:55 -0300

    Improvements to the Player script

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -13,53 +13,58 @@ var inputs = {
 	"ui_down": Vector2.DOWN
 }
 
-func _ready():
-	position = position.snapped(Vector2.ONE * tile_size)
-#	position += Vector2.ONE * tile_size/2 -- Doesnt work because of the player anchor
-
 var No_Input = preload("res://assets/No_Input.png")
 var Left_Input = preload("res://assets/Left_Input.png")
 var Right_Input = preload("res://assets/Right_Input.png")
 var Up_Input = preload("res://assets/Up_Input.png")
 var Down_Input = preload("res://assets/Down_Input.png")
+
+func _ready():
+	position = position.snapped(Vector2.ONE * tile_size)
+	# Doesnt work because the player sprite is not centered (offset).
+	# position += Vector2.ONE * tile_size/2
+
 func _unhandled_input(event):
+	# If the transition animation is happening, don't move the player
 	if tween.is_active():
 		return
-	for dir in inputs.keys():
-		if event.is_action_pressed(dir):
-			move(dir)
+
+	# Reset the game
 	if event.is_action_pressed("reset"):
 		get_tree().reload_current_scene()
-	#if event.is_action_pressed("pause"):
-		#var pause = load('res://Scenes/Pause_Menu.tscn').instance()
-		#get_tree().current_scene.add_child(pause)
+
+	# Pause the game
 	if event.is_action_pressed("pause"):
 		get_tree().change_scene("res://Scenes/Pause_Menu.tscn")
-	if event.is_action_pressed("ui_left"):
-		get_parent().get_node("Inputs/Input").set_texture(Left_Input)
-	elif event.is_action_pressed("ui_right"):
-		get_parent().get_node("Inputs/Input").set_texture(Right_Input)
-	elif event.is_action_pressed("ui_up"):
-		get_parent().get_node("Inputs/Input").set_texture(Up_Input)
-	elif event.is_action_pressed("ui_down"):
-		get_parent().get_node("Inputs/Input").set_texture(Down_Input)
 
-# Same as the countdown code, there 100 is a better way to write this. It is again 12am though.
-# I would like to properly implement this in the future.
+	# Move the player
+	for dir in inputs.keys():
+		if event.is_action_pressed(dir):
+			match(dir):
+				"ui_left":
+					get_parent().get_node("Inputs/Input").set_texture(Left_Input)
+				"ui_right":
+					get_parent().get_node("Inputs/Input").set_texture(Right_Input)
+				"ui_up":
+					get_parent().get_node("Inputs/Input").set_texture(Up_Input)
+				"ui_down":
+					get_parent().get_node("Inputs/Input").set_texture(Down_Input)
+			move(dir)
 
 func move(dir):
-	var game = get_parent()
 	ray.cast_to = inputs[dir] * tile_size
 	ray.force_raycast_update()
-	if !ray.is_colliding():
-#		position += dir * tile_size
+	
+	# get_collider will return null if not colliding with anything.
+	var collider = ray.get_collider()
+	if !collider:
 		move_tween(dir)
-		game.moves += 1
+		# get_parent returns the parent node so we can access its `moves` property directly, there
+		# is no need to creater a variable for it since it is only being used here.
+		get_parent().moves += 1
 	else:
-		var collider = ray.get_collider()
 		if collider.is_in_group("boxes"):
 			collider.move(inputs[dir])
-			game.moves += 1
 
 func move_tween(dir):
 	tween.interpolate_property(self, "position",
